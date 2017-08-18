19e361a9c0e4c954e987aa56240d89d5c33369e3
jestersunborn <jestersunborn@gmail.com>
2017-08-19 00:56:09 +0300

    Rewrite core to es6

--- app/index.js
+++ app/index.js
@@ -1,193 +1,175 @@
-const http = require('http');
-const querystring = require('querystring');
-const cheerio = require('cheerio');
-const windows1251 = require('windows-1251');
-const EventEmitter = require('events');
+import http from 'http';
+import querystring from 'querystring';
+import cheerio from 'cheerio';
+import windows1251 from 'windows-1251';
+import EventEmitter from 'events';
 
-function RutrackerApi(data) {
-  this.host = 'rutracker.org';
-  this.login_path = '/forum/login.php';
-  this.search_path = '/forum/tracker.php';
-  this.download_path = '/forum/dl.php';
-  this.full_path = '/forum/viewtopic.php';
-  this.cookie = null;
-  this.parseData = true;
-
-  if (typeof data === 'object' && data.username && data.password) {
-    this.username = data.username;
-    this.password = data.password;
-    this.login();
-  }
-
-  return this;
-}
-
-RutrackerApi.prototype = new EventEmitter();
-
-RutrackerApi.prototype.login = function(username, password) {
-  const postData = querystring.stringify({
-    login_username: username || this.username,
-    login_password: password || this.password,
-    login: 'Вход'
-  });
-
-  const options = {
-    hostname: this.host,
-    port: 80,
-    path: this.login_path,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': postData.length
-    }
-  };
-
-  var that = this;
-  var req = http.request(options, function(res) {
-    if (res.statusCode == '302') {
-      that.cookie = res.headers['set-cookie'][0];
-      that.emit('login');
-    } else {
-      that.emit('login-error');
-    }
-  });
-
-  req.on('error', function(err) { that.emit('error', err); });
-  req.write(postData);
-  req.end();
-};
-
-RutrackerApi.prototype.search = function(_query, _callback) {
-  if (typeof this.cookie != 'string') {
-    throw Error('Unauthorized: Use `login` method first');
-  }
-  else if (typeof _query == 'undefined') {
-    throw TypeError('Expected at least one argument');
-  }
-
-  var callback = _callback || function() {},
-      query = encodeURIComponent(_query),
-      path = this.search_path + '?nm=' + query;
-
-  var options = {
-    hostname: this.host,
-    port: 80,
-    path: path,
-    method: 'POST',
-    headers: {
-      'Cookie': this.cookie
-    }
-  };
-
-  var that = this;
-  var req = http.request(options, function(res) {
-    if (res.statusCode == '200') {
-      var data = '';
-      res.setEncoding('binary');
-      res.on('data', function(x) {
-        data = data + windows1251.decode(x, {mode: 'html'});
-      });
-      res.on('end', function() {
-        if (that.parseData === true) {
-          that.parseSearch(data, callback);
-        } else {
-          callback(data);
-        }
-      });
-    }
-    else {
-      throw Error('HTTP code is ' + res.statusCode);
-    }
-  });
-
-  req.on('error', function(err) { that.emit('error', err); });
-  req.end();
-};
-
-RutrackerApi.prototype.download = function(_id, _callback) {
-  if (typeof this.cookie != 'string') {
-    throw Error('Unauthorized: Use `login` method first');
-  }
-  else if (typeof _id == 'undefined') {
-    throw TypeError('Expected at least one argument');
-  }
-
-  var callback = _callback || function() {},
-      path = this.download_path + '?t=' + _id;
-
-  var options = {
-    hostname: this.host,
-    port: 80,
-    path: path,
-    method: 'GET',
-    headers: {
-      'Cookie': this.cookie
-    }
-  };
-
-  var that = this;
-  var req = http.request(options, function(res) {
-    if (res.statusCode == '200') {
-          callback(res);
-    }
-    else {
-      throw Error('HTTP code is ' + res.statusCode);
-    }
-  });
-
-  req.on('error', function(err) { that.emit('error', err); });
-  req.end();
+const formatSize = (sizeInBytes) => {
+  const sizeInMegabytes = `${sizeInBytes / (1000 * 1000 * 1000)}`;
+  return `${sizeInMegabytes.slice(0, 4)} GB`;
 };
 
 
-RutrackerApi.prototype.parseSearch = function(rawHtml, callback) {
-  var $ = cheerio.load(rawHtml, {decodeEntities: false}),
-      tracks = $('#tor-tbl tbody').find('tr'),
-      results = [],
-      length = tracks.length,
-      bytes_in_gigabyte = 1024 * 1024 * 1024;
+class RutrackerApi extends EventEmitter {
+  constructor(username, password, parseData = true) {
+    super();
+    this.username = username;
+    this.password = password;
+    this.host = 'rutracker.org';
+    this.loginPath = '/forum/login.php';
+    this.searchPath = '/forum/tracker.php';
+    this.downloadPath = '/forum/dl.php';
+    this.fullPath = '/forum/viewtopic.php';
+    this.cookie = null;
+    this.parseData = parseData;
 
-  for (var i = 0; i < length; i++) {
-    // Ah-m... Couldn't find any better method
-    var document = tracks.find('td'),
-        state    = document.next(),
-        category = state.next(),
-        title    = category.next(),
-        author   = title.next(),
-        size     = author.next(),
-        seeds    = size.next(),
-        leechs   = seeds.next();
+    this.login(username, password);
+  }
 
-    results.push({
-      state    : state.attr('title'),
-      id       : title.find('div a').attr('data-topic_id'),
-      category : category.find('.f-name a').html(),
-      title    : title.find('div a ').html(),
-      author   : author.find('div a ').html(),
-      size     : formatSize( size.find('*').html() ),
-      seeds    : seeds.find('b').html(),
-      leechs   : leechs.find('b').html(),
-      url      : 'http://' + this.host + '/forum/' + title.find('div a').attr('href')
+  // Login method
+  login(username, password) {
+    // User data
+    const postData = querystring.stringify({
+      login_username: username || this.username,
+      login_password: password || this.password,
+      login: 'Вход',
     });
 
-    tracks = tracks.next();
+    const options = {
+      hostname: this.host,
+      port: 80,
+      path: this.loginPath,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': postData.length,
+      },
+    };
+
+    const req = http.request(options, (res) => {
+      if (res.statusCode.toString() === '302') {
+        this.cookie = res.headers['set-cookie'][0];
+        this.emit('login-success');
+      } else {
+        this.emit('login-error');
+      }
+    });
+
+    req.on('error', (err) => { this.emit('error', err); });
+    req.write(postData);
+    req.end();
   }
 
-  // Handle case where search has no results
-  results = results.filter(function(x) {
-    return typeof x.id !== 'undefined';
-  });
+  // Search
+  search(q, cb = () => {}) {
+    console.log(q);
+    if (typeof this.cookie !== 'string') {
+      throw Error('Unauthorized: Use `login` method first');
+    } else if (typeof q === 'undefined') {
+      throw TypeError('Expected at least one argument');
+    }
+    const query = encodeURIComponent(q);
+    const path = `${this.searchPath}?nm=${query}`;
 
-  if (callback) {
-    callback(results);
-  } else {
+    const options = {
+      hostname: this.host,
+      port: 80,
+      path,
+      method: 'POST',
+      headers: { Cookie: this.cookie },
+    };
+
+    const req = http.request(options, (res) => {
+      if (res.statusCode.toString() === '200') {
+        let data = '';
+        res.setEncoding('binary');
+        res.on('data', (x) => {
+          data += windows1251.decode(x, { mode: 'html' });
+        });
+        res.on('end', () => {
+          if (this.parseData === true) {
+            this.parseSearch(data, cb);
+          } else {
+            cb(data);
+          }
+        });
+      } else {
+        throw Error(`HTTP code is ${res.statusCode}`);
+      }
+    });
+    req.on('error', (err) => { this.emit('error', err); });
+    req.end();
+  }
+
+  // Download
+  download(id, cb = () => {}) {
+    if (typeof this.cookie !== 'string') {
+      throw Error('Unauthorized: Use `login` method first');
+    } else if (typeof id === 'undefined') {
+      throw TypeError('Expected at least one argument');
+    }
+    const path = `${this.downloadPath}?t=${id}`;
+
+    const options = {
+      hostname: this.host,
+      port: 80,
+      path,
+      method: 'GET',
+      headers: { Cookie: this.cookie },
+    };
+
+    const req = http.request(options, (res) => {
+      if (res.statusCode.toString() === '200') {
+        cb(res);
+      } else {
+        throw Error(`HTTP code is ${res.statusCode}`);
+      }
+    });
+
+    req.on('error', (err) => { this.emit('error', err); });
+    req.end();
+  }
+
+  // Parse search results
+  parseSearch(html, cb) {
+    const $ = cheerio.load(html, { decodeEntities: false });
+    let tracks = $('#tor-tbl tbody').find('tr');
+    let results = [];
+    const length = tracks.length;
+
+    // TODO: refactor this shit
+    for (let i = 0; i < length; i += 1) {
+      const document = tracks.find('td');
+      const state = document.next();
+      const category = state.next();
+      const title = category.next();
+      const author = title.next();
+      const size = author.next();
+      const seeds = size.next();
+      const leechs = seeds.next();
+
+      results.push({
+        state: state.attr('title'),
+        id: title.find('div a').attr('data-topic_id'),
+        category: category.find('.f-name a').html(),
+        title: title.find('div a ').html(),
+        author: author.find('div a ').html(),
+        size: formatSize(size.find('*').html()),
+        seeds: seeds.find('b').html(),
+        leechs: leechs.find('b').html(),
+        url: `http://${this.host}/forum/${title.find('div a').attr('href')}`,
+      });
+      tracks = tracks.next();
+    }
+
+    // Handle case where search has no results
+    results = results.filter(x => typeof x.id !== 'undefined');
+
+    if (cb) {
+      return cb(results);
+    }
     return results;
   }
+}
 
-  function formatSize(size_in_bytes) {
-    var size_in_megabytes = size_in_bytes / (1000 * 1000 * 1000);
-    return ('' + size_in_megabytes).slice(0, 4) + ' GB';
-  }
-};
-
-module.exports = RutrackerApi;
+export default RutrackerApi;
