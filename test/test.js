69a05104e8dad26ffeddcb7f5574979af1b98707
jestersunborn <jestersunborn@gmail.com>
2017-08-19 00:56:28 +0300

    Login and search test

--- test/test.js
+++ test/test.js
@@ -1,15 +1,21 @@
-console.log('\x1Bc');
 // helpfull packages
 require('colors');
 // core packages
-const Rutracker = require('../app/');
+const Rutracker = require('../build/').default;
 // Config
 const { username, password } = require('./config');
 
-const rutracker = new Rutracker();
 
-rutracker.login(username, password);
+const rutracker = new Rutracker(username, password);
 
-rutracker.addListener('login', () => {
+
+rutracker.addListener('login-success', () => {
   console.log(' Now u r logged in '.bgGreen.black);
+  rutracker.search('Inception', (res) => {
+    console.log(res);
+  });
+});
+
+rutracker.addListener('login-error', () => {
+  console.log(' Please check your username or password '.bgRed.white);
 });
