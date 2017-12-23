4b8425056e898d3562f63b0303388b17974f338a
jestersunborn <jestersunborn@gmail.com>
2017-12-23 23:11:49 +0200

    Update test file and remove colors from dependencies

--- test/test.js
+++ test/test.js
@@ -1,19 +1,18 @@
-// Helpers
-require('colors');
-
-// Core packages
+// Import builded Rutracker class
 const Rutracker = require('../lib');
 
-// Config
+// Config (import from JSON-config file './test/config.json')
 const { username, password } = require('./config');
 
+// Create an object of Rutracker
 const rutracker = new Rutracker();
 
+// Test ID
+const ID = '0123456789';
+
+// Write your tests here
+// Use README.md documentation to see how all APIs work
 rutracker.login(username, password)
-    .then(() => rutracker.getStats())
-    .then((res) => {
-        console.log(res);
-    })
-    .catch((err) => {
-        console.log(JSON.stringify(err));
-    });
+    .then(() => rutracker.getUserInfo(ID))
+    .then(console.log)
+    .catch(console.error);
